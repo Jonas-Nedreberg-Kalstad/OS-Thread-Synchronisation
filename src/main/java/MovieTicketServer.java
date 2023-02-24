17d55157503c2bf41bd49bfdc8a2986080eb3c6d
DESKTOP-EONCIAU\Jonas <jonasned@stud.ntnu.no>
2023-02-24 18:46:48 +0100

    First commit

---
+++ src/main/java/MovieTicketServer.java
@@ -0,0 +1,20 @@
+
+public class MovieTicketServer {
+
+  private int availableSeats;
+  private int numberOfSeats;
+
+  public MovieTicketServer() {
+    // TBD
+  }
+
+  public void bookTicket() {
+    // TBD
+
+    if ((availableSeats - numberOfSeats) < 0) {
+      // TBD
+    }
+
+    // TBD
+  }
+}
